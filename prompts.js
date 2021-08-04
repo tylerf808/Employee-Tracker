7fd6d6d083b2477d04d593d781dc4b0a5fdd8618
Tyler Fabian <tylerf808@gmail.com>
2021-08-04 13:23:41 -0400

    Changing format of prompts. Will launch from this file now

--- prompts.js
+++ prompts.js
@@ -1,77 +1,22 @@
 const inquirer = require('inquirer');
+const ctable = require('console.table');
+const mysql = require('mysql');
+const {
+    createRole,
+    updateEmployeeManager,
+    getManagersList,
+    findAllEmployeesByManager,
+    updateEmployeeManager,
+    createDepartment,
+    findAllEmployeesByDepartment,
+    viewDepartments,
+    viewRoles,
+    createEmployee
+} = require('./queries')
 
-function getAction() {
-    const answer = inquirer.prompt([
-        {
-            name: 'action',
-            type: 'list',
-            message: 'What would you like to do?',
-            choices: ['View Employees', 'View Departments', 'View Roles', 'Add Employee', 'Add Department', 'Add Role', 'Update Employee Role']
-        }
-    ]).then((answers) => {
-        return answers.action;
-    })
-    return answer;
-};
+async function viewEmployees() {
+    let employees = await findAllEmployees();
+    let table = ctable.getTable([], employees);
+    console.log(table);
+  }
 
-function newEmployeePrompt() {
-    const answer = inquirer.prompt([
-        {
-            name: 'fName',
-            type: 'input',
-            message: 'What is the employees first name?'
-        },
-        {
-            name: 'lName',
-            type: 'input',
-            message: 'What is the employees last name?'
-        },
-        {
-            name: 'department',
-            type: 'list',
-            message: 'What is the employees role?',
-            choices: ['Software Engineer', 'Sales Lead', 'Sales Person', 'Lead Engineer', 'Lawyer', 'Legal Team Lead']
-        }
-    ]).then((answers) => {
-        return answers;
-    })
-    return answer;
-}
-
-function newDepartment() {
-    const answer = inquirer.prompt([
-        {
-            name: 'department',
-            type: 'input',
-            message: 'What is the department called?',
-        }
-    ]).then((answers) => {
-        return answers.department;
-    })
-    return answer; 
-}
-
-function newRole() {
-    const answer = inquirer.prompt([
-        {
-            name: 'role',
-            type: 'input',
-            message: 'What is the role called?',
-        },
-        {
-            name: 'salary',
-            type: 'input',
-            message: 'What is the roles salary?'
-        },
-        {
-            name: 'department',
-            type: 'input',
-            message:'What is the department?'
-        }
-    ]).then((answers) => {
-        return answers.role;
-    })
-    return answer; 
-}
-
-module.exports = { newEmployeePrompt, getAction, newDepartment, newRole }
